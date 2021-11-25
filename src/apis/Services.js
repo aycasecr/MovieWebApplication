c8fe1e18de2fe6dabf4d3797c7adc33803732a02
Ayça Seçer <secerayca16@gmail.com>
2021-11-25 09:43:02 +0300

    last changes

--- src/apis/Services.js
+++ src/apis/Services.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-// import { TMDB_API_KEY } from "./key";
+import { TMDB_API_KEY } from "./key";
 
 export default axios.create({
     baseURL: "https://api.themoviedb.org/3",
     params: {
-        api_key: "374c33af371a54f60b20532a4a9e7791"
+        api_key: `${TMDB_API_KEY}`
     }
 });
--- src/apis/tmdbs/key.js
+++ src/apis/tmdbs/key.js
@@ -1,1 +1,1 @@
-export const TMDB_API_KEY = process.env.REACT_APP_TMDB_API_KEY;
+export const TMDB_API_KEY = process.env.REACT_APP_API_KEY;
